a87213056988ee053c90ca1d227b2d58eb27bd5d
nd <32167abc>
2015-08-07 23:01:37 +0300

    add parser

--- icfpc2015/scala/src/main/scala/ru/biocad/game/Solver.scala
+++ icfpc2015/scala/src/main/scala/ru/biocad/game/Solver.scala
@@ -42,24 +42,3 @@ object Solver {
   }
 
 }
-
-
-object Parser {
-
-  def parseSoleCommand(c: String): Direction = {
-    c match {
-      case _ if c == "W" => Direction.West
-      case _ if c == "E" => Direction.East
-      case _ if c == "SW" => Direction.SouthWest
-      case _ if c == "SE" => Direction.SouthEast
-      case _ if c == "CW" => Direction.RotateClock
-      case _ if c == "CCW" => Direction.RotateCounterClock
-      case _ => throw new IllegalArgumentException("NO WAYS TO GO!!! GO HOME AND DIE!!!!")
-    }
-  }
-
-  def parseString(c: String): Vector[Direction] = {
-    c.split(" ").map(parseSoleCommand).toVector
-  }
-
-}
