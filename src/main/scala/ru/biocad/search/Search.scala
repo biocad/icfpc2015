6511c74d57e69f43f00fbd374f7a3620bd84bcb6
nd <32167abc>
2015-08-08 02:38:14 +0300

    uniform cost

--- src/main/scala/ru/biocad/search/Search.scala
+++ src/main/scala/ru/biocad/search/Search.scala
@@ -20,6 +20,7 @@ object Search {
     val stack = new mutable.Queue[(Cell, ArrayBuffer[Cell])]()
     val solution = new ArrayBuffer[Cell]()
 
+    @annotation.tailrec
     def go(state: (Cell, ArrayBuffer[Cell])): ArrayBuffer[Cell] = {
       if (state._1 == problem.goalCell) {
         state._2
@@ -57,5 +58,28 @@ object Search {
     }
 
     go((problem.startCell, solution))
+  }
+
+  def uniformCostSearch(problem: Problem): ArrayBuffer[Cell] = {
+
+    val closed = new ArrayBuffer[Cell]()
+    val queue = new mutable.PriorityQueue[(Cell, ArrayBuffer[Cell], Double)]()
+    val solution = new ArrayBuffer[Cell]()
+
+    @annotation.tailrec
+    def go(state: (Cell, ArrayBuffer[Cell], Double)): ArrayBuffer[Cell] = {
+      if (state._1 == problem.goalCell) {
+        state._2
+      }
+      else {
+        if (!closed.contains(state._1)) closed.append(state._1)
+        problem.successors(state._1).foreach {
+          case (c, s) => queue.enqueue((c, s :+c, 0))
+        }
+        go(queue.dequeue())
+      }
+    }
+
+    go((problem.startCell, solution, 0))
   }
 }
