87ec4c002cc0541a92db7d800440eb40f15f57e5
Pavel Yakovlev <zhmactep@gmail.com>
2015-08-07 18:28:26 +0300

    Board

--- icfpc2015/scala/src/main/scala/ru/biocad/game/Board.scala
+++ icfpc2015/scala/src/main/scala/ru/biocad/game/Board.scala
@@ -5,6 +5,7 @@ package ru.biocad.game
  * Date: 07.08.15
  * Time: 19:17
  */
-class Board(id : Int)(width : Int, height : Int)(filled : ) {
-
+class Board(id : Int)(width : Int, height : Int)(filled : Iterable[Cell]) {
+  def inBoard(cell : Cell) : Boolean =
+    cell.x >= 0 && cell.x < width && cell.y >= 0 && cell.y < height
 }
