a79397e4bf023985c1301db564b72ae61360e156
Pavel Yakovlev <zhmactep@gmail.com>
2015-08-07 16:08:19 +0300

    Board added

--- icfpc2015/solver.py
+++ icfpc2015/solver.py
@@ -2,18 +2,23 @@ __author__ = 'pavel'
 
 import argparse
 import json
+import sys
 
-def load_input():
-    return {}
+from board import Board
 
+def load_board(json_string):
+    d = json.loads(json_string)
+    board = Board(d["id"], d["height"], d["width"], d["filled"], d["sourceLength"])
+    print(board.__dict__, file=sys.stderr)
+    return board
 
-def play(input_dict):
+def play(board):
     return "ctulhu"
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', dest='FILENAME', metavar='FILENAME', nargs='+', help='File containing JSON encoded input.')
+    parser.add_argument('-f', dest='FILENAME', required=True, metavar='FILENAME', nargs='+', help='File containing JSON encoded input.')
     parser.add_argument('-t', dest='TIME', metavar='NUMBER', help='Time limit, in seconds, to produce output')
     parser.add_argument('-m', dest='MEMORY', metavar='NUMBER', help='Memory limit, in megabytes, to produce output')
     parser.add_argument('-p', dest='PHRASE', metavar='STRING', help='Phrase of power, as quoted string')
@@ -22,7 +27,12 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print(args)
+    print(args, file=sys.stderr)
+    for filename in args.FILENAME:
+        with open(filename, "rt") as fd:
+            s = fd.readlines()
+        s = "".join(s)
+        print(play(load_board(s)))
 
 
 if __name__ == "__main__":
