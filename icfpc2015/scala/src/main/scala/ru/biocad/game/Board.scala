1cfb4e5637132b80f9704b9c46da6822b424622e
nd <nd.startup@gmail.com>
2015-08-07 22:43:29 +0300

    solver???

--- icfpc2015/scala/src/main/scala/ru/biocad/game/Board.scala
+++ icfpc2015/scala/src/main/scala/ru/biocad/game/Board.scala
@@ -1,5 +1,7 @@
 package ru.biocad.game
 
+import ru.biocad.game.Direction.Direction
+
 /**
  * User: pavel
  * Date: 07.08.15
@@ -8,10 +10,19 @@ package ru.biocad.game
 class Board(id : Int)(val width : Int, val height : Int) {
   def inBoard(cell : Cell) : Boolean =
     cell.x >= 0 && cell.x < width && cell.y >= 0 && cell.y < height
+
+  def cells: Vector[Cell] = {
+    val r = for {
+      x <- 0 to width
+      y <- 0 to height
+    } yield new Cell(x, y)
+
+    r.toVector
+  }
 }
 
 case class BoardState(filled : Vector[Cell])(board : Board) {
-  def intersects(cell : Cell) : Boolean =
+  private def intersects(cell : Cell) : Boolean =
     filled.contains(cell)
 
   def update(bee : Bee) : BoardState =
@@ -22,14 +33,22 @@ case class BoardState(filled : Vector[Cell])(board : Board) {
       BoardState(filled = newField(bee))(board = board)
     }
 
-  def newField(bee : Bee) : Vector[Cell] = {
+  private def newField(bee : Bee) : Vector[Cell] = {
     (filled ++ bee.members).distinct.groupBy(_.y).flatMap {
       case (_, cells) =>
         Option(cells).filter(_.size < board.width)
     }.flatten.toVector
   }
 
-  def dumps : String =
+  def initNewBee(bee: Bee): BoardState = {
+    this.update(bee)
+  }
+
+  def getNextState(bee: Bee, command: Direction): BoardState = {
+    this.update(bee.move(command))
+  }
+
+  def dump : String =
     s"""
       |{
       |  "width": ${board.width},
