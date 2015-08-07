6314286ee0aff0b40377dd544efd18cab402f34e
nd <32167abc>
2015-08-07 16:53:57 +0300

    add problem definition

--- icfpc2015/search.py
+++ icfpc2015/search.py
@@ -2,6 +2,49 @@
 from icfpc2015 import util
 
 
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        raise NotImplemented
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        raise NotImplemented
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        raise NotImplemented
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        raise NotImplemented
+
+
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
