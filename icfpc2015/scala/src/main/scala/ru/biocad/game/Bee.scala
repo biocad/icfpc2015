63e9b6ff3677ccba751b4676dbef99d8e237d7cb
Pavel Yakovlev <zhmactep@gmail.com>
2015-08-07 20:44:16 +0300

    Bee movement

--- icfpc2015/scala/src/main/scala/ru/biocad/game/Bee.scala
+++ icfpc2015/scala/src/main/scala/ru/biocad/game/Bee.scala
@@ -1,4 +1,14 @@
 package ru.biocad.game
 
-class Bee(val members: Vector[Cell], val pivot: Cell) {
+import ru.biocad.game.Direction.Direction
+
+case class Bee(members: Vector[Cell], pivot: Cell) {
+  def move : Direction => Bee = {
+    case Direction.RotateClock =>
+      Bee(members = members.map(_.rotate(pivot)(Direction.RotateClock)), pivot = pivot)
+    case Direction.RotateConterClock =>
+      Bee(members = members.map(_.rotate(pivot)(Direction.RotateConterClock)), pivot = pivot)
+    case mv =>
+      Bee(members = members.map(_.move(mv)), pivot.move(mv))
+  }
 }
