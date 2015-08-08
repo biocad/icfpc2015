dcd035dbacaea1e4051fd0b20f834c1db59d31fc
nd <nd.startup@gmail.com>
2015-08-08 21:05:12 +0300

    kiss my shiny metal scorer

--- src/main/scala/ru/biocad/game/Scorer.scala
+++ src/main/scala/ru/biocad/game/Scorer.scala
@@ -4,22 +4,26 @@ object Scorer {
 
   def getScore(currentGameState: GameState, nextGameState: GameState): Long = {
 
-    val size = 1 // currentGameState.bee.members.size
-    val currentFilled = currentGameState.boardState.filled.groupBy(c => c.r)
-    val nextFilled = nextGameState.boardState.filled.groupBy(c => c.r)
+    if (currentGameState.boardState.filled.length != nextGameState.boardState.filled.length) {
+      val size = currentGameState.bee.members.size
+      val currentFilled = currentGameState.boardState.filled.groupBy(c => c.r)
+      val nextFilled = nextGameState.boardState.filled.groupBy(c => c.r)
 
-    val currentFilledSize = currentFilled.size
-    val nextFilledSize = nextFilled.size
+      val currentFilledSize = currentFilled.size
+      val nextFilledSize = nextFilled.size
 
-    val ls = math.max(currentFilledSize - nextFilledSize, 0)
+      val ls = math.max(currentFilledSize - nextFilledSize, 0)
 
-    val points = size + 100 * (1 + ls) * ls / 2
+      val points = size + 100 * (1 + ls) * ls / 2
 
-    val ls_old = ls
+      val ls_old = ls
 
-    val lineBonus = if (ls_old > 1) math.floor((ls_old - 1) * points / 10).toLong else 0
+      val lineBonus = if (ls_old > 1) math.floor((ls_old - 1) * points / 10).toLong else 0
 
-    points + lineBonus
+      points + lineBonus
+    } else {
+      0
+    }
   }
 
 }
