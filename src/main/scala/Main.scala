26dfd39f2d6a0b254b25d2970eed2b49a7a0d329
Pavel Yakovlev <zhmactep@gmail.com>
2015-08-08 12:08:33 +0300

    Morning wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import ru.biocad.game.{BoardState, Game, GameState}
+import ru.biocad.game.{Move, BoardState, Game, GameState}
 import ru.biocad.util.Parser
 
 /**
@@ -10,25 +10,28 @@ object Main extends App {
 
   override def main(args : Array[String]) = {
 
-    val rawProblem = scala.io.Source.fromFile("problems/problem_0.json").mkString
+    val rawProblem = scala.io.Source.fromFile("problems/problem_6.json").mkString
 
     val parsedProblem = Parser.parseProblem(rawProblem)
 
     val (board, filled, beezArray) = parsedProblem.getGameRules
 
-    beezArray.foreach {
+    beezArray.take(1).foreach {
       case beez =>
         val game = new Game(board)
         val initial = GameState(boardState = BoardState(filled)(board), bee = beez.head, beez = beez, currentBee = 0)
 
-        "45ppppp45ppppp".foldLeft(Option(initial)) {
+        var i = 0
+        "iiiiiiimimiiiiiimmimiiiimimimmimimimimmeemmimimiimmmmimmimiimimimmimmimeee\nmmmimimmimeeemiimiimimimiiiipimiimimmmmeemimeemimimimmmmemimmimmmiiimmmiii\npiimiiippiimmmeemimiipimmimmipppimmimeemeemimiieemimmmm".foldLeft(Option(initial)) {
           case (st, ch) =>
             st match {
               case Some(state) =>
+                i += 1
                 val newState = game.movement(state)(ch)
-                print(newState.get.dumpJson)
                 newState
-              case None => None
+              case None =>
+                println(i)
+                None
             }
         }
     }
---
+++ submit.sh
@@ -0,0 +1,3 @@
+OUTPUT=$(cat $1)
+#echo $OUTPUT
+echo "curl --user :RVm6OOelIARr4U0Vi39X/fjCcU1YOOmjbZGTFEBzZ98= -X POST -H "Content-Type: application/json" -d "$OUTPUT" https://davar.icfpcontest.org/teams/24/solutions"
