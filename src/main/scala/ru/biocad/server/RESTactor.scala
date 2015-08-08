cc05fd9cf842827d93b107ce6aa1acd322eaffad
Pavel Yakovlev <zhmactep@gmail.com>
2015-08-08 12:55:32 +0300

    Server alive

--- src/main/scala/ru/biocad/server/RESTactor.scala
+++ src/main/scala/ru/biocad/server/RESTactor.scala
@@ -1,15 +1,14 @@
 package ru.biocad.server
 
 import akka.actor.Actor
-import ru.biocad.game.GameState
-import spray.routing.HttpService
+import spray.routing._
 
 /**
  * User: pavel
  * Date: 08.08.15
  * Time: 13:24
  */
-class RESTactor(updateFoo : Char => Option[GameState]) extends Actor with HttpService {
+class RESTactor extends Actor with HttpService {
   override def actorRefFactory = context
 
   override def receive = runRoute(myRoute)
