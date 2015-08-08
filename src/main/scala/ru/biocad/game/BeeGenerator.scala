35e7ea7f31035a3480528add9c638c758f30af9d
nd <32167abc>
2015-08-08 15:03:46 +0300

    kiss my shiny metal generator

--- src/main/scala/ru/biocad/game/BeeGenerator.scala
+++ src/main/scala/ru/biocad/game/BeeGenerator.scala
@@ -1,5 +1,34 @@
 package ru.biocad.game
 
+trait LCG {
+  def generate(seeds: Array[Int], length: Int): Array[Array[Int]] = {
+    val modulus = math.pow(2, 31).toInt
+    val multiplier = 1103515245
+    val increment = 12345
+    seeds.map { seed =>
+      var gseed = seed
+      Iterator.range(0, length).map { _ =>
+        val cb = (gseed >> 16) & 0x7fff
+        gseed = (multiplier * gseed + increment) % modulus
+        cb
+      }.toArray
+    }
+  }
+}
+
+
+object LCGTest extends App with LCG {
+
+  val s = new Array[Int](1)
+  s(0) = 17
+  val asdf = generate(s, 10).flatten
+
+  println(asdf.mkString(" "))
+
+  assert(asdf sameElements Array(0, 24107, 16552, 12125, 9427, 13152, 21440, 3383, 6873, 16117))
+}
+
+
 trait BeeGenerator {
     def generate(bees: Array[Bee], seeds: Array[Int], length: Int): Array[Array[Bee]] = {
         val modulus = math.pow(2, 31).toInt
@@ -9,7 +38,7 @@ trait BeeGenerator {
         seeds.map { seed =>
             var gseed = seed
             Iterator.range(0, length).map { _ =>
-                val cb = bees((gseed >> 16) & 0x7fff % ulen)
+                val cb = bees(((gseed >> 16) & 0x7fff) % ulen)
                 gseed = (multiplier * gseed + increment) % modulus
                 cb
             }.toArray
