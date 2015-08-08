336e4238d205e86570568783d3d967b9ac329c65
nd <nd.startup@gmail.com>
2015-08-08 13:23:14 +0300

    deep-10 dirty hack

--- src/main/scala/ru/biocad/search/Search.scala
+++ src/main/scala/ru/biocad/search/Search.scala
@@ -10,7 +10,7 @@ class Problem(val startCell: Cell, val goalCell: Cell, val bee: Bee, val boardSt
   def successors(state: Cell): Vector[(Cell, ArrayBuffer[Move])] = {
     getMoves(state)
   }
-  
+
   def moveWithHistory(start: Cell, direction: Direction): (Cell, ArrayBuffer[Direction]) = {
     (start.move(direction), ArrayBuffer(direction))
   }
@@ -20,38 +20,49 @@ class Problem(val startCell: Cell, val goalCell: Cell, val bee: Bee, val boardSt
   }
 
   def getMoves(start: Cell): Vector[(Cell, ArrayBuffer[Move])] = {
-    
+
     val possibleDirections: Vector[Direction] = Vector(West, East, SouthWest, SouthEast)
 
     def getNeighbours(cell: Cell): Vector[(Cell, Move)] = possibleDirections.map(d => (cell.move(d), d))
 
     // 1-deep search
-    getNeighbours(start).map(cd => (cd._1, ArrayBuffer(cd._2)))
+    val deep1 = getNeighbours(start).map(cd => (cd._1, ArrayBuffer(cd._2)))
+    val deep2 = deep1.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep3 = deep2.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep4 = deep3.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep5 = deep4.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep6 = deep5.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep7 = deep6.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep8 = deep7.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep9 = deep8.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+    val deep10 = deep9.flatMap { case (c, history) => getNeighbours(c).map { case (n, m) => (n, history :+ m) } }
+
+    deep10
   }
 
 
-//    function cube_reachable(start, movement):
+  //    function cube_reachable(start, movement):
   //    var visited = set()
   //    add start to visited
   //    var fringes = []
   //    fringes.append([start])
-//
-//    for each 1 < k ≤ movement:
-//      fringes.append([])
+  //
+  //    for each 1 < k ≤ movement:
+  //      fringes.append([])
   //    for each cube in fringes[k-1]:
-    //    for each 0 ≤ dir < 6:
-      //    var neighbor = cube_neighbor(cube, dir)
-      //    if neighbor not in visited, not blocked:
-      //      add neighbor to visited
-      //        fringes[k].append(neighbor)
-//
-//    return visited
+  //    for each 0 ≤ dir < 6:
+  //    var neighbor = cube_neighbor(cube, dir)
+  //    if neighbor not in visited, not blocked:
+  //      add neighbor to visited
+  //        fringes[k].append(neighbor)
+  //
+  //    return visited
 
   def getAllowedMoves(bee: Bee, boardState: BoardState): Array[(Cell, Move)] = {
-    Move.all.map{
-      case d : Direction =>
+    Move.all.map {
+      case d: Direction =>
         (d, bee.pivot.move(d), boardState.isLocked(Bee(bee.members.map(_.move(d)), bee.pivot.move(d))))
-      case r : Rotation =>
+      case r: Rotation =>
         (r, bee.pivot, boardState.isLocked(Bee(bee.members.map(_.rotate(bee.pivot, r)), bee.pivot.rotate(bee.pivot, r))))
     }.filter(_._3 == false).map(o => (o._2, o._1)).toArray
   }
